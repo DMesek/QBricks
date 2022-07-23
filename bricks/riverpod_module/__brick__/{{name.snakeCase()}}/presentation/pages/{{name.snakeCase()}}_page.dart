356a33187590062c9600ce0f6ff840d8400c70f6
dmesek <dominik.mesek@q.agency>
2022-07-23 16:33:28 +0200

    added generated page

---
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/presentation/pages/{{name.snakeCase()}}_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../domain/notifiers/{{name.snakeCase()}}_notifier.dart';
+
+class {{name.pascalCase()}}Page extends ConsumerWidget {
+  const {{name.pascalCase()}}Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch({{name.camelCase()}}NotifierProvider);
+    return state.when(
+      data: ({{entity.camelCase()}}) => Container(),
+      loading: () => Container(),
+      error: () => Container(),
+    );
+  }
+}
