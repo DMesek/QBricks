1931fc3a114ba03d83f306cc6398355aab070bae
dmesek <dominik.mesek@q.agency>
2022-07-23 15:43:47 +0200

    fixed model generation

--- bricks/riverpod_module/__brick__/{{name.snakeCase()}}/data/models/{{entity.snakeCase()}}_response.dart
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/data/models/{{entity.snakeCase()}}_response.dart
@@ -4,7 +4,9 @@ part '{{entity.snakeCase()}}_response.g.dart';
 
 @JsonSerializable()
 class {{entity.pascalCase()}}Response {
+  final String property;
   const {{entity.pascalCase()}}Response({
+    required this.property,
   });
 
   factory {{entity.pascalCase()}}Response.fromJson(Map<String, dynamic> json) =>
