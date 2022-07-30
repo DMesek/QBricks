e3d57825e49e3110f8573f07343d81a42a4b0eea
dmesek <dominik.mesek@q.agency>
2022-07-30 16:19:43 +0200

    added beamer builder

---
+++ bricks/clean_feature/__brick__/lib/feature/{{name.snakeCase()}}/{{#useBeamer}}{{name.snakeCase()}}_location.dart
@@ -0,0 +1,20 @@
+import 'package:beamer/beamer.dart';
+
+class {{name.pascalCase()}}Location extends BeamLocation<BeamState> {
+
+  static const {{name.camelCase()}} = '/{{name.camelCase()}}';
+
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    return [
+      const BeamPage(
+        name: {{name.camelCase()}},
+        key: const ValueKey({{name.camelCase()}}),
+        child: {{name.pascalCase()}}Page(),
+      ),
+    ];
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [{{name.camelCase()}}];
+}
