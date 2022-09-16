09cc8d409e2c3bb3b9529cc606ac83320f8d1ce2
dmesek <dominik.mesek@q.agency>
2022-09-16 10:08:02 +0200

    added project name var to all bricks

--- add-feature.sh
+++ add-feature.sh
@@ -1,14 +1,20 @@
 if [ -z "$1" ]; then 
+    echo "No project name provided."; exit 1
+fi
+if [ -z "$2" ]; then 
     echo "No feature name provided."; exit 1
 fi
-feature_name="$1"
+project_name="$1"
+feature_name="$2"
 
 [ ! -f mason.yaml ] && echo "No mason.yaml. Please run mason init" && exit 1
 [ ! -f mason-lock.json ] && echo "No mason-lock.json. Please run mason get" && exit 1
 
 echo "Setup your entity:"
-mason make entity --feature_name $feature_name
+mason make entity --feature_name $feature_name --project_name $project_name
 echo "Setup your respone/request model:"
-mason make model --feature_name $feature_name
+mason make model --feature_name $feature_name --project_name $project_name
 echo "Setup your repository:"
-mason make repository --feature_name $feature_name
+mason make repository --feature_name $feature_name --project_name $project_name
+echo "Setup your notifier:"
+mason make repository --feature_name $notifier --project_name $project_name
